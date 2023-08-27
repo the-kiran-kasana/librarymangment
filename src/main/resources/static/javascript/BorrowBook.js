2848819ff6d9347e594661665fcfc15f6eabc2aa
Kiran Kasana <mkasana@Kirans-MacBook-Air.local>
2023-08-27 13:19:26 +0530

    get_all_book_from_server

--- src/main/resources/static/javascript/BorrowBook.js
+++ src/main/resources/static/javascript/BorrowBook.js
@@ -4,16 +4,33 @@ function getBooks() {
 }
 
 
-function getBooksFromServer() {
+function getBooksAndList() {
     var result = null;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            // Return books details from server
-            if (this.responseText != null) {
-                result = this.responseText;
+      if (this.readyState == 4 && this.status == 200) {
+          // Return books details from server
+          if (this.responseText != null) 
+          {
+            bookListString = this.responseText;
+            console.log(bookListString);
+            var parseResponse = JSON.parse(bookListString);
+            var buildDivForBooks = null;
+            for (i in parseResponse)
+            {
+              if (buildDivForBooks == null) 
+              {
+                  buildDivForBooks = getDiv(parseResponse[i].book_id, parseResponse[i].book_name)
+              } else 
+              {
+                 buildDivForBooks += getDiv(parseResponse[i].book_id, parseResponse[i].book_name)
+              }
             }
-        } else {
+             document.getElementById("booksList").innerHTML = buildDivForBooks;
+            }
+         }
+        else 
+        {
             // Return hardcoded books
             result = getBooks()
         }
@@ -22,29 +39,36 @@ function getBooksFromServer() {
     xhttp.setRequestHeader("Accept", "application/json");
     xhttp.setRequestHeader("Content-Type", "application/json");
     xhttp.send();
-    return result;
+    //return result;
 }
 
-function getBooksAndList() {
-    var bookListString = getBooksFromServer();
-    var parseResponse = JSON.parse(bookListString);
-    var buildDivForBooks = null;
-    for (i in parseResponse) {
-        if (buildDivForBooks == null) {
-            buildDivForBooks = getDiv(parseResponse[i].book_id, parseResponse[i].book_name)
-        } else {
-            buildDivForBooks += getDiv(parseResponse[i].book_id, parseResponse[i].book_name)
-        }
-    }
-    document.getElementById("booksList").innerHTML = buildDivForBooks;
-}
+
+
+// function getBooksAndList() {
+//     var bookListString = getBooksFromServer();
+//     var parseResponse = JSON.parse(bookListString);
+//     var buildDivForBooks = null;
+//     for (i in parseResponse) {
+//         if (buildDivForBooks == null) {
+//             buildDivForBooks = getDiv(parseResponse[i].book_id, parseResponse[i].book_name)
+//         } else {
+//             buildDivForBooks += getDiv(parseResponse[i].book_id, parseResponse[i].book_name)
+//         }
+//     }
+//     document.getElementById("booksList").innerHTML = buildDivForBooks;
+// }
+
+
 
 function getDiv(bookId, bookName) {
     return "<div style=\"border: 1px solid #ddd; padding: 10px; display: flex;  width: 200px; background-color: #722212; margin-top: 20px;\">\n" +
-    "        <img src=\"../static/images/book_" + bookId + ".png\" alt=\"Book Cover\" style=\"width: 100px; height: auto; margin-right: 10px; background-color: aqua;\">\n" +
+    "        <img src=\"/images/book_" + bookId + ".png\" alt=\"Book Cover\" style=\"width: 100px; height: auto; margin-right: 10px; background-color: aqua;\">\n" +
     "        <div style=\"font-family: Arial, sans-serif; background-color: #04AA6D;\">\n" +
+    "             <br>"+
     "            <h3>" + bookName + "</h3>\n" +
-    "            <p>Book Id : " + bookId + "</p>\n" +
+    "            <p>Book_id : " + bookId + "</p>\n" +
     "        </div>\n" +
     "    </div>";
 }
+
+
